4a97b9bbd9f17956215c54464dccd86c8fd7946f
BJ Lilley <bethjeanlilley@gmail.com>
2020-07-20 16:48:40 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,25 @@
 class Pokemon
+  
+  attr_accessor :id, :name, :type, :db
+  
+  def initialize(id:, name:, type:, db:) 
+    @id = id
+    @name = name
+    @type = type 
+    @db = db 
+  end 
+  
+  def self.save(name, type, database_connection) 
+    sql = <<-SQL 
+    INSERT INTO pokemon (name, type)
+    VALUES (?,?)
+    SQL
+    database_connection.execute(sql)
+    
+    #@id = DB[:conn].execute("MAX(ID) AS LastId FROM pokemons")[0][0]
+  end 
+  
+  
+  
+  
 end
